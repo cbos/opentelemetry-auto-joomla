9581e292a73c208d3628721a268d2f59135f646f
Cees Bos <dev@ceesbos.nl>
2024-10-24 22:05:25 +0200

    First auto instrumentation working with a demo setup

--- src/JoomlaInstrumentation.php
+++ src/JoomlaInstrumentation.php
@@ -28,5 +28,65 @@ class JoomlaInstrumentation
             null,
             'https://opentelemetry.io/schemas/1.24.0'
         );
+
+        self::_hook($instrumentation, 'Joomla\CMS\Factory', 'getContainer', 'Joomla.Factory', SpanKind::KIND_SERVER);
+//        self::_hook($instrumentation, 'SiteApplication', 'execute', 'Joomla.SiteApplication.cl', SpanKind::KIND_SERVER);
+//        self::_hook($instrumentation, 'Joomla\CMS\Application\SiteApplication', 'execute', 'Joomla.SiteApplication.cl', SpanKind::KIND_SERVER);
+//        self::_hook($instrumentation, 'DatabaseInterface', 'connect', 'Joomla.database.connect', SpanKind::KIND_CLIENT);
+//        self::_hook($instrumentation, 'DatabaseInterface', 'disconnect', 'Joomla.database.disconnect', SpanKind::KIND_CLIENT);
+    }
+
+    /**
+     * Simple generic hook function which starts and ends a minimal span
+     * @psalm-param SpanKind::KIND_* $spanKind
+     */
+    private static function _hook(CachedInstrumentation $instrumentation, ?string $class, string $function, string $name, int $spanKind = SpanKind::KIND_INTERNAL): void
+    {
+        hook(
+            class: $class,
+            function: $function,
+            pre: static function ($object, ?array $params, ?string $class, ?string $function, ?string $filename, ?int $lineno) use ($instrumentation, $name, $spanKind) {
+                $span = self::builder($instrumentation, $name, $function, $class, $filename, $lineno)
+                    ->setSpanKind($spanKind)
+                    ->startSpan();
+                Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+            },
+            post: static function ($object, ?array $params, mixed $return, ?Throwable $exception) {
+                self::end($exception);
+            }
+        );
+    }
+
+    private static function builder(
+        CachedInstrumentation $instrumentation,
+        string $name,
+        ?string $function,
+        ?string $class,
+        ?string $filename,
+        ?int $lineno,
+    ): SpanBuilderInterface {
+        /** @psalm-suppress ArgumentTypeCoercion */
+        return $instrumentation->tracer()
+            ->spanBuilder($name)
+            ->setAttribute(TraceAttributes::CODE_FUNCTION, $function)
+            ->setAttribute(TraceAttributes::CODE_NAMESPACE, $class)
+            ->setAttribute(TraceAttributes::CODE_FILEPATH, $filename)
+            ->setAttribute(TraceAttributes::CODE_LINENO, $lineno);
+    }
+
+    private static function end(?Throwable $exception): void
+    {
+        $scope = Context::storage()->scope();
+        if (!$scope) {
+            return;
+        }
+        $scope->detach();
+        $span = Span::fromContext($scope->context());
+        if ($exception) {
+            $span->recordException($exception, [TraceAttributes::EXCEPTION_ESCAPED => true]);
+            $span->setStatus(StatusCode::STATUS_ERROR, $exception->getMessage());
+        }
+
+        $span->end();
     }
 }
