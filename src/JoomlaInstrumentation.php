5732bb6275963b3aa39c84dee3078c9fb06cce7f
Cees Bos <dev@ceesbos.nl>
2024-10-28 22:03:13 +0100

    Add instrumentation for root span and Site application and Admin application internal spans.

--- src/JoomlaInstrumentation.php
+++ src/JoomlaInstrumentation.php
@@ -29,18 +29,92 @@ class JoomlaInstrumentation
             'https://opentelemetry.io/schemas/1.24.0'
         );
 
-        self::_hook($instrumentation, 'Joomla\CMS\Factory', 'getContainer', 'Joomla.Factory', SpanKind::KIND_SERVER);
-//        self::_hook($instrumentation, 'SiteApplication', 'execute', 'Joomla.SiteApplication.cl', SpanKind::KIND_SERVER);
-//        self::_hook($instrumentation, 'Joomla\CMS\Application\SiteApplication', 'execute', 'Joomla.SiteApplication.cl', SpanKind::KIND_SERVER);
-//        self::_hook($instrumentation, 'DatabaseInterface', 'connect', 'Joomla.database.connect', SpanKind::KIND_CLIENT);
-//        self::_hook($instrumentation, 'DatabaseInterface', 'disconnect', 'Joomla.database.disconnect', SpanKind::KIND_CLIENT);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\SiteApplication', 'execute', 'Joomla.SiteApplication.execute', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\SiteApplication', 'doExecute', 'Joomla.SiteApplication.doExecute', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\SiteApplication', 'render', 'Joomla.SiteApplication.render', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\SiteApplication', 'route', 'Joomla.SiteApplication.route', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\SiteApplication', 'dispatch', 'Joomla.SiteApplication.dispatch', SpanKind::KIND_INTERNAL);
+
+        self::_hook($instrumentation, 'Joomla\CMS\Application\AdministratorApplication', 'execute', 'Joomla.AdministratorApplication.execute', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\AdministratorApplication', 'doExecute', 'Joomla.AdministratorApplication.doExecute', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\AdministratorApplication', 'render', 'Joomla.AdministratorApplication.render', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\AdministratorApplication', 'route', 'Joomla.AdministratorApplication.route', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\AdministratorApplication', 'dispatch', 'Joomla.AdministratorApplication.dispatch', SpanKind::KIND_INTERNAL);
+
+        //self::_hook($instrumentation, 'Joomla\CMS\Application\CMSApplication', 'execute', 'Joomla.CMSApplication.execute', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\CMSApplication', 'render', 'Joomla.CMSApplication.render', SpanKind::KIND_INTERNAL);
+        self::_hook($instrumentation, 'Joomla\CMS\Application\CMSApplication', 'respond', 'Joomla.CMSApplication.respond', SpanKind::KIND_INTERNAL);
+
+
+        //CMSApplication execute method execute once and initiates all actions and plugins
+        hook(
+            class: 'Joomla\CMS\Application\CMSApplication',
+            function: 'execute',
+            pre: static function ($object, ?array $params, ?string $class, ?string $function, ?string $filename, ?int $lineno) use ($instrumentation) {
+                $factory = new Psr17Factory();
+                $request = (new ServerRequestCreator($factory, $factory, $factory, $factory))->fromGlobals();
+                $parent = Globals::propagator()->extract($request->getHeaders());
+
+                $span = $instrumentation
+                    ->tracer()
+                    ->spanBuilder(sprintf('%s %s', $request->getMethod(), $request->getUri()->getPath()))
+                    ->setParent($parent)
+                    ->setSpanKind(SpanKind::KIND_SERVER)
+                    ->setAttribute(TraceAttributes::URL_FULL, (string)$request->getUri())
+                    ->setAttribute(TraceAttributes::URL_SCHEME, $request->getUri()->getScheme())
+                    ->setAttribute(TraceAttributes::URL_PATH, $request->getUri()->getPath())
+                    ->setAttribute(TraceAttributes::HTTP_REQUEST_METHOD, $request->getMethod())
+                    ->setAttribute(TraceAttributes::NETWORK_PROTOCOL_VERSION, $request->getProtocolVersion())
+                    ->setAttribute(TraceAttributes::USER_AGENT_ORIGINAL, $request->getHeaderLine('User-Agent'))
+                    ->setAttribute(TraceAttributes::HTTP_REQUEST_BODY_SIZE, $request->getHeaderLine('Content-Length'))
+                    ->setAttribute(TraceAttributes::CLIENT_ADDRESS, $request->getUri()->getHost())
+                    ->setAttribute(TraceAttributes::CLIENT_PORT, $request->getUri()->getPort())
+                    ->startSpan();
+                Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+            },
+            post: static function ($object, ?array $params, mixed $return, ?Throwable $exception) {
+
+                $scope = Context::storage()->scope();
+                if (!$scope) {
+                    return;
+                }
+                $span = Span::fromContext($scope->context());
+
+                foreach ($object->getHeaders() as $header) {
+                    if ('status' == strtolower($header['name'])) {
+                        $status = (int)$header['value'];
+                        $span->setAttribute(TraceAttributes::HTTP_RESPONSE_STATUS_CODE, $status);
+                    }
+                }
+                self::end($exception);
+            }
+        );
+
+        /**
+         * Create a span for every db query. This can get noisy, so could be turned off via config?
+         */
+        hook(
+            class: 'Joomla\Database\DatabaseInterface',
+            function: 'execute',
+            pre: static function ($object, ?array $params, ?string $class, ?string $function, ?string $filename, ?int $lineno) use ($instrumentation) {
+                $span = self::builder($instrumentation, 'Joomla.database.execute', $function, $class, $filename, $lineno)
+                    ->setSpanKind(SpanKind::KIND_CLIENT)
+                    ->setAttribute(TraceAttributes::DB_STATEMENT, $object->getQuery(false)->__toString())
+                    ->setAttribute(TraceAttributes::DB_SYSTEM, $object->getServerType())
+                    ->startSpan();
+                Context::storage()->attach($span->storeInContext(Context::getCurrent()));
+            },
+            post: static function ($object, ?array $params, mixed $return, ?Throwable $exception) {
+                self::end($exception);
+            }
+        );
     }
 
     /**
      * Simple generic hook function which starts and ends a minimal span
      * @psalm-param SpanKind::KIND_* $spanKind
      */
-    private static function _hook(CachedInstrumentation $instrumentation, ?string $class, string $function, string $name, int $spanKind = SpanKind::KIND_INTERNAL): void
+    private static function _hook(CachedInstrumentation $instrumentation, ?string $class, string $function, string $name, int $spanKind): void
     {
         hook(
             class: $class,
@@ -59,12 +133,13 @@ class JoomlaInstrumentation
 
     private static function builder(
         CachedInstrumentation $instrumentation,
-        string $name,
-        ?string $function,
-        ?string $class,
-        ?string $filename,
-        ?int $lineno,
-    ): SpanBuilderInterface {
+        string                $name,
+        ?string               $function,
+        ?string               $class,
+        ?string               $filename,
+        ?int                  $lineno,
+    ): SpanBuilderInterface
+    {
         /** @psalm-suppress ArgumentTypeCoercion */
         return $instrumentation->tracer()
             ->spanBuilder($name)
